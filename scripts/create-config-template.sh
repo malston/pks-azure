8f96cb0a5942fe91bb8fe80684a2668ae22030be
Mark Alston <malston@pivotal.io>
2020-04-01 14:56:22 -0600

    Add script to create config template

---
+++ scripts/create-config-template.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+
+__DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ -z "$ENVIRONMENT_NAME" ]; then
+    echo "Must provide environment name ENVIRONMENT_NAME as environment variable"
+    echo "Set this to the same value of environment_name var in terraform.tfvars"
+    exit 1
+fi
+
+if [ -z "$PIVNET_TOKEN" ]; then
+    echo "Must provide pivnet api token as environment variable PIVNET_TOKEN"
+    exit 1
+fi
+
+version=$(om interpolate --config "${__DIR}/../versions.yml" --path /pks_version)
+files_directory="${__DIR}/../pcf/files/$ENVIRONMENT_NAME"
+
+mkdir -p "${files_directory}"
+
+om config-template \
+    --output-directory "${files_directory}" \
+    --pivnet-api-token "$PIVNET_TOKEN" \
+    --pivnet-product-slug "pivotal-container-service" \
+    --product-version "$version" \
+    --pivnet-file-glob "pivotal-container-service*.pivotal"
