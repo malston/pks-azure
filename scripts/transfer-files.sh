f9ffe6b54c9584952bdd02da82045c0302b7c361
Mark Alston <malston@pivotal.io>
2020-04-02 10:40:10 -0600

    Fix scp copy

--- scripts/transfer-files.sh
+++ scripts/transfer-files.sh
@@ -15,16 +15,18 @@ fi
 state_file=${__DIR}/../pcf/state/"$ENVIRONMENT_NAME"/terraform.tfstate
 opsman_dns=$(terraform output -state="${state_file}" ops_manager_dns)
 private_key="$HOME"/.ssh/om_pks_azure_ssh_key
-[[ ! -f ${private_key} ]] && echo "opsman ssh private key written to '${private_key}'"
 
-terraform output -state="${state_file}" ops_manager_ssh_private_key > "${private_key}"
-chmod 600 "${private_key}"
+if [[ ! -f ${private_key} ]]; then
+	terraform output -state="${state_file}" ops_manager_ssh_private_key > "${private_key}"
+	chmod 400 "${private_key}"
+	 echo "opsman ssh private key written to '${private_key}'"
+fi
 
 scp -i "${private_key}" "$HOME/.ssh/id_rsa" \
 	ubuntu@"${opsman_dns}":/home/ubuntu/.ssh
 
 scp -i "${private_key}" -r "${__DIR}/../pcf/state" \
-	ubuntu@"${opsman_dns}":/home/ubuntu/workspace/pks-azure/pcf/state
+	ubuntu@"${opsman_dns}":/home/ubuntu/workspace/pks-azure/pcf/
 
 scp -i "${private_key}" -r "${__DIR}/../pcf/vars" \
-	ubuntu@"${opsman_dns}":/home/ubuntu/workspace/pks-azure/pcf/vars
+	ubuntu@"${opsman_dns}":/home/ubuntu/workspace/pks-azure/pcf/
